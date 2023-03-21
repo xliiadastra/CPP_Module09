956e549241cd771c28afbea7da2ac8e1bf348c9d
Yihyun Choi <yichoi@c3r4s4.42seoul.kr>
2023-03-21 18:21:47 +0900

    1

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEEXCHANGE_HPP
+
+# include <iostream>
+# include <fstream>
+# include <string>
+
+
+class BitcoinExchange {
+private:
+
+
+
+public:
+
+}
+
+
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,25 @@
+#include "bitcoinExchange.hpp"
+
+int	main(int argc, char* argv[])
+{
+	if (argc == 2)
+	{
+		ifstream ifs(argv[1]); // ios::app, ios::binary
+		if (!argv[1])
+		{
+			std::cerr << "Cannot open file " << std::endl;
+			exit(1);
+		}
+		while (ifs)
+		{
+			std::string	str;
+			getline(ifs, str);
+
+			std::cout << str << std::endl;
+		}
+	}
+	else
+		std::cout << "Bad Argument: just one input '.csv' file name" << std::endl;
+
+	return (0);
+}
