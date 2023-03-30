3964c27d2b9ebb116932455e068d94611d93467b
Yihyun Choi <yichoi@c3r4s2.42seoul.kr>
2023-03-30 21:56:44 +0900

    database

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,2 +1,21 @@
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange()
+{
+	// data.csv file open -> input database
+	database();
+}
+
+//BitcoinExchange::BitcoinExchange(std::string m_file_name)
+//{
+	// 'file_name.csv' file open -> input database
+//}
+
+BitcoinExchange::~BitcoinExchange()
+{
+	// delete database
+}
+
+map<int, char>&	BitcoinExchange::make_database()
+{
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -7,12 +7,17 @@
 
 class BitcoinExchange {
 private:
-	int m_fd;
-	BitcoinExchange() {};
-	~BitcoinExchange() {};
+//	int 	m_fd;
+//	string	m_file_name;
+	map<int, char> database;
+	BitcoinExchange(BitcoinExchange obj) { if (this != obj) m_file_name = obj->m_file_name; };
 
 public:
-	BitcoinExchange(int fd) { m_fd = fd; };
+	BitcoinExchange();
+//	BitcoinExchange(std::string file_name);
+	~BitcoinExchange();
+
+	map<int, char>& make_database();
 };
 
 #endif
